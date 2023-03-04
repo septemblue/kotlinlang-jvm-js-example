96bcf143b8e8ab50af9522d85c4dc7b42de0bfad
septemblue <focusramxram@gmail.com>
2023-03-04 20:07:42 +0700

    add get, post, delete for ShoppingList Path

--- src/jvmMain/kotlin/plugins/Routing.kt
+++ src/jvmMain/kotlin/plugins/Routing.kt
@@ -2,26 +2,38 @@ package plugins
 
 import ShoppingListItem
 import Store
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
-    val shoppingList = Store().shoppingList
-    shoppingList.addAll(
-        listOf(
-            ShoppingListItem("Cucumbers 🥒", 1),
-            ShoppingListItem("Tomatoes 🍅", 2),
-            ShoppingListItem("Orange Juice 🍊", 3)
-        )
+    val shoppingList = mutableListOf(
+        ShoppingListItem("Cucumbers 🥒", 1),
+        ShoppingListItem("Tomatoes 🍅", 2),
+        ShoppingListItem("Orange Juice 🍊", 3)
     )
+
     routing {
         get("/") {
             call.respondText { "Hello World" }
         }
+        /* Routes are grouped based on a common path. You don't have to specify the route path as a String.
+        Instead, the path from the ShoppingListItem model is used
+        */
         route(ShoppingListItem.path) {
             get {
                 call.respond(shoppingList)
+            }
+            post {
+                shoppingList += call.receive<ShoppingListItem>()
+                call.respond(HttpStatusCode.OK)
+            }
+            delete("/{id}") {
+                val id = call.parameters["id"]?.toInt() ?: error("Invalid delete request")
+                shoppingList.removeIf { it.id == id}
+                call.respond(HttpStatusCode.OK)
             }
         }
     }
